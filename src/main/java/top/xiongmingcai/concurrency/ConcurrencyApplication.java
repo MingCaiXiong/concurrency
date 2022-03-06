fee2a9748ffa916d0f5479d0a28dc7670e8bd57e
xmc <xmc000@icloud.com>
2022-03-06 17:45:24 +0800

    案例环境准备

--- src/main/java/top/xiongmingcai/concurrency/ConcurrencyApplication.java
+++ src/main/java/top/xiongmingcai/concurrency/ConcurrencyApplication.java
@@ -2,8 +2,10 @@ package top.xiongmingcai.concurrency;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "top.xiongmingcai.*")
 public class ConcurrencyApplication {
 
     public static void main(String[] args) {
--- src/main/java/top/xiongmingcai/controller/TestController.java
+++ src/main/java/top/xiongmingcai/controller/TestController.java
@@ -1,2 +1,16 @@
-package top.xiongmingcai.controller;public class TestControoller {
+package top.xiongmingcai.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class TestController {
+
+    @RequestMapping("/stat")
+    public String test(){
+        return "stat";
+    }
+
 }
